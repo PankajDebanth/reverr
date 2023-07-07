994d0309e2f8d6ba97889f9ebdeccb6723d371d1
Pankaj Debnath <126234805+PankajDebanth@users.noreply.github.com>
2023-07-07 16:46:42 +0530

    Add files via upload

---
+++ src/Home.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import Counter from './Counter';
+import './Home.css';
+
+const Home = () => {
+  const [counters, setCounters] = useState([]);
+
+  const addCounter = () => {
+    setCounters([...counters, <Counter key={counters.length} />]);
+  };
+
+  const removeCounter = (index) => {
+    const updatedCounters = [...counters];
+    updatedCounters.splice(index, 1);
+    setCounters(updatedCounters);
+  };
+
+  return (
+    <div className="home-container">
+      <h1 className="home-title">Counting Web App</h1>
+      {counters.map((counter, index) => (
+        <div className="counter-wrapper" key={index}>
+          {counter}
+          <button className="close-button" onClick={() => removeCounter(index)}>
+            &times;
+          </button>
+        </div>
+      ))}
+      <button className="add-counter-button" onClick={addCounter}>
+        Add Counter
+      </button>
+    </div>
+  );
+};
+
+export default Home;
